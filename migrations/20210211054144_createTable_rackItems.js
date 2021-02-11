a884d8c6a4982aa930e6d9180dcc100864a9b495
Ryan Chase <ryan@chasery.com>
2021-02-11 07:28:24 -0800

    Create rack items migrations and seeds

---
+++ migrations/20210211054144_createTable_rackItems.js
@@ -0,0 +1,25 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('ru_rack_items', function (table) {
+    table.increments('item_id');
+    table.string('item_name', 255).notNullable();
+    table.decimal('item_price', 14, 2).notNullable();
+    table.string('item_url', 255);
+    table.integer('user_id').unsigned().notNullable();
+    table
+      .foreign('user_id')
+      .references('id')
+      .inTable('ru_users')
+      .onDelete('CASCADE');
+    table.integer('rack_id').unsigned().notNullable();
+    table
+      .foreign('rack_id')
+      .references('rack_id')
+      .inTable('ru_racks')
+      .onDelete('CASCADE');
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('ru_rack_items');
+};
--- seeds/seed.racking_up.js
+++ seeds/seed.racking_up.js
@@ -1,5 +1,6 @@
 exports.seed = async function (knex, Promise) {
   // Delete table data
+  await knex('ru_rack_items').del();
   await knex('ru_racks').del();
   await knex('ru_users').del();
   // Reset ru_users auto increment back to 1
@@ -28,43 +29,108 @@ exports.seed = async function (knex, Promise) {
       password: '$2a$12$hEh1U/A2dpGG3tFjTW95iOB3Yasr7Ib/bal5AZfUyJwj0noFgWn0y',
       name: 'Dwight',
     },
-    {
-      email: 'ryan@howard.com',
-      password: '$2a$12$eiwkEPAR2GdRb9PWBJcf6uF/6rwgtBALqDZrd1JX3qzGFoz68PeAa',
-      name: 'R Howard',
-    },
   ]);
   // Insert racks into table
   await knex('ru_racks').insert([
     {
-      rack_id: 1,
       rack_name: 'Office Wear',
       user_id: 1,
     },
     {
-      rack_id: 2,
       rack_name: "Michael's Run",
       user_id: 1,
     },
     {
-      rack_id: 3,
-      rack_name: 'Rack 3',
+      rack_name: "Magician's Outfit",
       user_id: 2,
     },
     {
-      rack_id: 4,
-      rack_name: 'Rack 4',
+      rack_name: 'Waterfall Wedding Wear',
       user_id: 3,
     },
     {
-      rack_id: 5,
-      rack_name: 'Rack 5',
-      user_id: 4,
+      rack_name: 'Art School',
+      user_id: 3,
+    },
+  ]);
+  // Insert rack items into table
+  await knex('ru_rack_items').insert([
+    {
+      item_name: 'Yellow Button Down',
+      item_price: 49.99,
+      item_url: '',
+      user_id: 1,
+      rack_id: 1,
     },
     {
-      rack_id: 6,
-      rack_name: 'Rack 6',
-      user_id: 5,
+      item_name: 'Blazer Jacket',
+      item_price: 109.99,
+      item_url: '',
+      user_id: 1,
+      rack_id: 1,
+    },
+    {
+      item_name: 'Fake Glasses',
+      item_price: 24.99,
+      item_url: '',
+      user_id: 1,
+      rack_id: 1,
+    },
+    {
+      item_name: 'Top Hat',
+      item_price: 79.99,
+      item_url: '',
+      user_id: 2,
+      rack_id: 3,
+    },
+    {
+      item_name: 'Black Cloak',
+      item_price: 59.99,
+      item_url: '',
+      user_id: 2,
+      rack_id: 3,
+    },
+    {
+      item_name: 'Bunny',
+      item_price: 80.0,
+      item_url: '',
+      user_id: 2,
+      rack_id: 3,
+    },
+    {
+      item_name: 'Beautiful Dress',
+      item_price: 1099.99,
+      item_url: '',
+      user_id: 3,
+      rack_id: 4,
+    },
+    {
+      item_name: 'Reception Shoes',
+      item_price: 101.99,
+      item_url: '',
+      user_id: 3,
+      rack_id: 4,
+    },
+    {
+      item_name: 'Square Famed Glasses',
+      item_price: 89.99,
+      item_url: '',
+      user_id: 3,
+      rack_id: 5,
+    },
+    {
+      item_name: 'Cute Blouse',
+      item_price: 49.99,
+      item_url: '',
+      user_id: 3,
+      rack_id: 5,
+    },
+    {
+      item_name: 'Pencil Skirt',
+      item_price: 59.99,
+      item_url: '',
+      user_id: 3,
+      rack_id: 5,
     },
   ]);
 };
